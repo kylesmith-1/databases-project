673719646eb5127f8fe552149f91f53aa9dfaf89
Kyle Smith <kas.smith2000@gmail.com>
2021-04-27 22:49:46 -0400

    changing config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,24 +4,24 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url="ecto://db_proj:atlan99@localhost/db_proj"
-  # System.get_env("DATABASE_URL") ||
-  #   raise """
-  #   environment variable DATABASE_URL is missing.
-  #   For example: ecto://USER:PASS@HOST/DATABASE
-  #   """
+database_url=
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
 config :db_proj, DbProj.Repo,
   # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base = "sPS/BNgFluYN6m5kXznEC7zaX3IGllAYHiB0MLFrps2b0X7PvAk4ABC9KG50NP/n"
-  # System.get_env("SECRET_KEY_BASE") ||
-  #   raise """
-  #   environment variable SECRET_KEY_BASE is missing.
-  #   You can generate one by calling: mix phx.gen.secret
-  #   """
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :db_proj, DbProjWeb.Endpoint,
   http: [
